53f38cb9fa92177dfbcfaf76c1c9dc0df87b20f4
Theodor Arbus <theodorarbus@hotmail.com>
2025-03-12 16:29:36 +0100

    Flyttat alla DB-anrop till TicketToCodeDbcontext istället för applicationdbcontext. Buggfix i Get.cs också

--- TicketToCode.Core/Data/DbContext.cs
+++ TicketToCode.Core/Data/DbContext.cs
@@ -11,7 +11,7 @@ public class TicketToCodeDbContext : DbContext
     }
 
     public DbSet<Event> Events { get; set; }
-    public DbSet<User> Users { get; set; }
+    //public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
