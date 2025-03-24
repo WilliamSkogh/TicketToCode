27e12dfbc7b631882867dada717fe9c9c9d7b6c8
WilliamSkogh <skogh22@gmail.com>
2025-03-24 18:40:18 +0100

    buggfix

---
+++ TicketToCode.Api/Migrations/20250317103819_FixUserColumnName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketToCode.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                newName: "IX_Users_username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_username",
+                table: "Users",
+                newName: "IX_Users_Username");
+        }
+    }
+}
