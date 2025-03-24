9f92e7dc25bd74ef5d1291440af2070a02411353
Theodor Arbus <theodorarbus@hotmail.com>
2025-03-24 16:32:16 +0100

    Bytte endpoints nu funkar eventregistrering

--- MyBlazorApp/Services/EventService.cs
+++ MyBlazorApp/Services/EventService.cs
@@ -14,12 +14,16 @@ public class EventService
 
     public async Task<List<Event>?> GetEventsAsync()
     {
-        return await _httpClient.GetFromJsonAsync<List<Event>>("api/events");
+        return await _httpClient.GetFromJsonAsync<List<Event>>("/events");
     }
 
     public async Task<bool> CreateEventAsync(Event newEvent)
     {
-        var response = await _httpClient.PostAsJsonAsync("api/events", newEvent);
+        var response = await _httpClient.PostAsJsonAsync("/events", newEvent);
         return response.IsSuccessStatusCode;
+    }
+    public async Task<HttpResponseMessage> CreateEventRawAsync(Event newEvent)
+    {
+        return await _httpClient.PostAsJsonAsync("/events", newEvent);
     }
 }
